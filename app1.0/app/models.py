302b35c1dc22b101c5797529c0540a1ef36d6bb3
efe <victorefe5@gmail.com>
2023-10-15 11:41:50 +0100

    First working app

---
+++ app1.0/app/models.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from app import db
+from flask_login import UserMixin
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+    role = db.Column(db.String(64))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
+    password_reset_token = db.Column(db.String(128))
+
+    # Set and verify password methods
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    # Representation method for debugging
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
